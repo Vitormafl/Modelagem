e35ec233d7b8abca8a7a39fdb5e2ba031bc7c70c
GH120 <90730714+GH120@users.noreply.github.com>
2023-05-29 13:35:09 -0300

    Quicksort consertado, falta mudar o .h

--- Sorts/sorts.cpp
+++ Sorts/sorts.cpp
@@ -33,23 +33,31 @@ void quickSort(vector<pontoAngulo>& arr, int low, int high) {
     }
 }
 
-int partition(vector<Ponto>& pontos, int low, int high) {
-    int pivot = pontos[high].x; // Escolhe o último ponto como pivô
+
+void trocar(Ponto& a, Ponto& b) {
+    Ponto temp = a;
+    a = b;
+    b = temp;
+}
+
+int particionar(vector<Ponto>& pontos, int low, int high) {
+    double pivot = pontos[high].x;
     int i = (low - 1);
 
     for (int j = low; j <= high - 1; j++) {
-        if (pontos[j].x <= pivot) {
+        if (pontos[j].x < pivot) {
             i++;
-            swap(pontos[i], pontos[j]);
+            trocar(pontos[i], pontos[j]);
         }
     }
-    swap(pontos[i + 1], pontos[high]);
+
+    trocar(pontos[i + 1], pontos[high]);
     return (i + 1);
 }
 
 void quicksort(vector<Ponto>& pontos, int low, int high) {
     if (low < high) {
-        int pi = partition(pontos, low, high);
+        int pi = particionar(pontos, low, high);
 
         quicksort(pontos, low, pi - 1);
         quicksort(pontos, pi + 1, high);
